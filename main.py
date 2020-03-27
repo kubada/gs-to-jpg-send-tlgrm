2d7a438245ba45fc1870affbd1b008d4048f9e2e
Danya Violet <29613388+violet1one@users.noreply.github.com>
2020-03-27 16:54:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from pdf2image import convert_from_path
 import requests
 import os
+from sys import platform
 
+if platform == "linux" or platform == "linux2":
+    pass
+    path = 'files/'
+elif platform == "win32":
+    poppler = r'C:\Users\Violet\PycharmProjects\gs-to-jpg-send-tlgrm\poppler\bin'
+    path = r'C:\Users\Violet\PycharmProjects\gs-to-jpg-send-tlgrm\files\\'
+
+# Google Spreadsheet
 # Param export GS
 # &format=pdf                   //export format
 # &size=a4                      //A3/A4/A5/B4/B5/letter/tabloid/legal/statement/executive/folio
@@ -21,13 +30,6 @@ import os
 # &fzr=false                    //true/false
 # &fzc=false                    //true/false
 # &attachment=false             //true/false
-
-poppler = r'C:\Users\Violet\PycharmProjects\gs-to-jpg-send-tlgrm
-\poppler\bin'
-save_folder = r'C:\Users\Violet\PycharmProjects\gs-to-jpg-send-tlgrm
-\files'
-
-# Google Spreadsheet
 gs_id = '1QJRxp...gVVBIw'
 gs_url = 'https://docs.google.com/spreadsheets/d/' + gs_id + '/export?format=pdf&gid='
 gs_param = '&size=a5&portrait=false&vertical_alignment=MIDDLE&horizontal_alignment=CENTER'
@@ -41,22 +43,22 @@ proxy = {'https': 'socks5h://LOGIN:PASS@IP:1080'}
 def main(gid):
     # Download Google Spreadsheet
     response = requests.get(gs_url + str(gid) + gs_param)
-    with open(r'files\test-table.pdf', 'wb') as out_file:
+    with open(path + 'test-table.pdf', 'wb') as out_file:
         for chunk in response:
             out_file.write(chunk)
 
     # Google Spreadsheet pdf to jpg
-    convert_from_path(r'files\test-table.pdf', poppler_path=poppler, output_folder=save_folder,
+    convert_from_path(path + 'test-table.pdf', poppler_path=poppler, output_folder=path,
                       output_file='test-table', fmt='jpg')
 
     # Send Google spreadsheet jpg send from bot
     requests.post('https://api.telegram.org/bot' + bot + '/sendPhoto',
-                  files={'photo': open(r'files\test-table0001-1.jpg', 'rb')},
+                  files={'photo': open(path + 'test-table0001-1.jpg', 'rb')},
                   data={'chat_id': 'id_tlgrm'}, proxies=proxy)
 
 
 for gid in gs_table:
     main(gid)
 
-os.remove(r'files\test-table.pdf')
-os.remove(r'files\test-table0001-1.jpg')
+os.remove(path + 'test-table.pdf')
+os.remove(path + 'test-table0001-1.jpg')
